7c41450a2bbc3721b8a5caf02bc783f7e9db5d03
AlexZhadaev <pvt.vatnik@gmail.com>
2021-08-31 21:57:32 +0300

    Refactoring of networking

---
+++ Networking/Networking/MainCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MainCollectionViewCell.swift
+//  Networking
+//
+//  Created by Жадаев Алексей on 31.08.2021.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+}
